828e643ed4f24fee53b5abb85b5f0a618190207a
IvanAvila1224 <magista1224@gmail.com>
2023-03-02 11:32:53 -0600

    netflix

--- titanic.py
+++ titanic.py
@@ -6,48 +6,38 @@ titanic_link = 'https://raw.githubusercontent.com/adsoftsito/nosql/main/csv/tita
 
 titanic_data = pd.read_csv(titanic_link)
 
-# Create the title for the web app
-st.title("My First Streamlit App")
-sidebar = st.sidebar
-sidebar.title("This is the sidebar.")
-sidebar.write("You can add any elements to the sidebar.")
+fig, ax = plt.subplots()
 
-# Give user the current date
+ax.hist(titanic_data.fare)
 
-today = datetime.date.today()
-today_date = st.date_input('Current date', today)
+st.header("Histograma del Titanic")
 
-st.success('Current date: `%s`' % (today_date))
-
-# Display the content of the dataset if checkbox is true
-
-st.header("Dataset")
-agree = sidebar.checkbox("show DataSet Overview ? ")
-if agree:
-  st.dataframe(titanic_data)
-
-  # Select the embark town of the passanger and then display the dataset with this selection
-selected_town = sidebar.radio("Select Embark Town", titanic_data['embark_town'].unique())
-st.write("Selected Embark Town:", selected_town)
-
-st.write(titanic_data.query(f"""embark_town==@selected_town"""))
+st.pyplot(fig)
 
 st.markdown("___")
 
-# Select a range of the fare and then display the dataset with this selection
-optionals = st.expander("Optional Configurations", True)
-fare_min = optionals.slider(
-    "Minimum Fare",
-    min_value=float(titanic_data['fare'].min()),
-    max_value=float(titanic_data['fare'].max())
-)
-fare_max = optionals.slider(
-    "Maximum Fare",
-    min_value=float(titanic_data['fare'].min()),
-    max_value=float(titanic_data['fare'].max())
-)
-subset_fare = titanic_data[(titanic_data['fare'] <= fare_max) & (fare_min <= titanic_data['fare'])]
-st.write(f"Number of Records With Fare Between {fare_min} and {fare_max}: {subset_fare.shape[0]}")
+fig2, ax2 = plt.subplots()
 
-# Display of the dataset 
-st.dataframe(subset_fare)
+y_pos = titanic_data['class']
+x_pos = titanic_data['fare']
+
+ax2.barh(y_pos, x_pos)
+ax2.set_ylabel("Class")
+ax2.set_xlabel("Fare")
+ax2.set_title('¿Cuanto pagaron las clases del Titanic')
+
+st.header("Grafica de Barras del Titanic")
+
+st.pyplot(fig2)
+
+st.markdown("___")
+
+fig3, ax3 = plt.subplots()
+
+ax3.scatter(titanic_data.age, titanic_data.fare)
+ax3.set_xlabel("Edad")
+ax3.set_ylabel("Tarifa")
+
+st.header("Grafica de Dispersión del Titanic")
+
+st.pyplot(fig3)
